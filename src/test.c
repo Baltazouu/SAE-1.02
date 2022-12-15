fb2676baa66957f6797f9206f12a1fe2b8b596dc
Alexandre Agostinho <alexandre.agostinho@etu.uca.fr>
2022-12-15 22:01:21 +0100

    test of acutest

--- src/test.c
+++ src/test.c
@@ -3,12 +3,30 @@
 
 void test_add(void)
 {
-    TEST_CHECK_(add(2, 2)==(2+2), "add(%d,%d)==%d", 2, 2, (2+2));
+    int expected[] = { 2, 4, 7, -2, 0 };
+    int returned[] = {
+        add(1, 1),
+        add(2, 2),
+        add(1, 2),
+        add(-4, 3),
+        add(-4, 4)
+    };
+
+    for (int i = 0; i < 5; i++) {
+        TEST_CHECK_(returned[i] == expected[i], "add(%d)", i);
+        TEST_MSG("Expected: %d", expected[i]);
+        TEST_MSG("Returned: %d", returned[i]);
+    }
 }
 
 void test_supp(void)
 {
-    TEST_CHECK_(supp(3, 2)==(3-2), "supp(%d,%d)==%d", 3, 2, (3-2));
+    int expected = 1;
+    int returned = supp(3, 2);
+    TEST_CHECK_(returned==1, "supp(3, 2)");
+
+    TEST_MSG("Expected: %d", expected);
+    TEST_MSG("Returned: %d", returned);
 }
 
 
